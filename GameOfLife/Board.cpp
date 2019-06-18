741c05c9cffa5a5dd218b29a8ae4973c0033dd44
Jakub Cieplinski <cieplinski.jakubsp@gmail.com>
2019-06-18 13:52:19 +0200

    some refactoring

--- GameOfLife/Board.cpp
+++ GameOfLife/Board.cpp
@@ -161,42 +161,42 @@ void Board::calculateNewBoardState(std::vector<std::vector<std::shared_ptr<Cell>
         for(auto width = 0; width < m_Board[0].size(); width++)
         {
 			auto tmp = m_Board[height][width]->getState();
-            // First most cases
-                    if(0 < height && 0 < width && m_Board.size()-1 > width && m_Board[1].size()-1 > height)
-                    {
-                        neighbours.resize(8);
+					// First most cases
+			if(0 < height && 0 < width && m_Board.size()-1 > width && m_Board[1].size()-1 > height)
+            {
+				neighbours.resize(8);
 
-                        for(auto i = -1; i <= 1; i++)
+				for(auto i = -1; i <= 1; i++)
+                {
+					for(auto j = -1; j <= 1; j++)
+                    {
+						if(not (0 == j and 0 == i)) // don't pass the cell itself to check
                         {
-                            for(auto j = -1; j <= 1; j++)
-                            {
-                                if(not (0 == j and 0 == i)) // don't pass the cell itself to check
-                                {
-                                    neighbours.push_back(m_Board[height+i][width+j]->getState()); // get all cells around
-                                }
-                            }
+							neighbours.push_back(m_Board[height+i][width+j]->getState()); // get all cells around
                         }
-                        tempBoard[height][width]->changeState(Cell::determineNewState(neighbours, tmp));
-                        neighbours.clear();
                     }
+                 }
+				tempBoard[height][width]->changeState(Cell::determineNewState(neighbours, tmp));
+                neighbours.clear();
+			}
+
             //First row
             if(height == 0)
             {
+				neighbours.resize(3);
+
                 // Upper left corner
                 if(width == 0)
                 {
-                    neighbours.resize(3);
-
                     neighbours = {m_Board[height + 1][width + 1]->getState(), m_Board[height + 1][width]->getState(),
                                   m_Board[height][width + 1]->getState()};
                     tempBoard[height][width]->changeState(Cell::determineNewState(neighbours, tmp));
                     neighbours.clear();
                 }
+
                 //Upper right corner
                 if(width == m_Board[0].size()-1)
                 {
-                    neighbours.resize(3);
-
                     neighbours = {m_Board[height + 1][width - 1]->getState(), m_Board[height + 1][width]->getState(),
                                   m_Board[height][width - 1]->getState()};
                     tempBoard[height][width]->changeState(Cell::determineNewState(neighbours, tmp));
@@ -206,20 +206,21 @@ void Board::calculateNewBoardState(std::vector<std::vector<std::shared_ptr<Cell>
             //Last row
             if(height == m_Board.size()-1)
             {
+				neighbours.resize(3);
+
                 //Lower left corner
                 if(width == 0)
                 {
-                    neighbours.resize(3);
 
                     neighbours = {m_Board[height - 1][width + 1]->getState(), m_Board[height - 1][width]->getState(),
                                   m_Board[height][width + 1]->getState()};
                     tempBoard[height][width]->changeState(Cell::determineNewState(neighbours, tmp));
                     neighbours.clear();
                 }
+
                 //Lower right corner
                 if(width == m_Board[0].size()-1)
                 {
-                    neighbours.resize(3);
 
                     neighbours = {m_Board[height - 1][width - 1]->getState(), m_Board[height - 1][width]->getState(),
                                   m_Board[height][width - 1]->getState()};
